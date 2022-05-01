eb14372b778966c94e63524f96a817cdc3d876fe
SwatiTParshuramkar <zarinaparshuramkar23@gmail.com>
2022-05-01 11:56:55 +0530

    Recycler View

---
+++ app/src/main/java/com/example/android/affirmation/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.android.affirmation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.affirmation.Adapter.ItemAdapter
+import com.example.android.affirmation.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Initialize Data
+        val myDataset = Datasource().loadAffirmation()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter= ItemAdapter(this,myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
+    }
+}
